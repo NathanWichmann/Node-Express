d1b688a0197b9d65102c1af7a59b4dece04f6262
Nathan_Wichmann <nathanwicker@hotmail.com>
2022-12-29 15:27:45 -0500

    review

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const _ = require('lodash')
+const EventEmitter = require('events')
 
+const customEmitter = new EventEmitter()
+customEmitter.on('response', () => {
+    console.log(`data recieved`)
+})
 
-const items = [1, [2, [3, [4 ]]]]
-
-const newItems = _.flattenDeep(items)
-
-console.log(newItems)
+customEmitter.emit('response')
